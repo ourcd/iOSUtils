5dc1f701ca7f37b43176a7b28bdd4f03f469ab51
ccf <ccf@ccfs-MacBook-Pro.local>
2013-03-23 09:39:31 +0800

    add custom tabbarcontroller

---
+++ utils/FTabBarViewController.m
@@ -0,0 +1,52 @@
+//
+//  FTabBarViewController.m
+//  CarInfo
+//
+//  Created by ccf on 3/23/13.
+//  Copyright (c) 2013 ccf. All rights reserved.
+//
+
+#import "FTabBarViewController.h"
+
+@interface FTabBarViewController ()
+
+@end
+
+@implementation FTabBarViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - rorate
+- (BOOL)shouldAutorotate {
+    return NO;
+}
+
+- (NSUInteger)supportedInterfaceOrientations {
+    return UIInterfaceOrientationMaskPortrait;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation{
+    return (toInterfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+
+@end
